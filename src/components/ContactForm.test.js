bf7e60b4a3ea50a339dcc4c61e6353538d147139
Nithya Narayanan <n.nithya@pm.me>
2022-01-13 17:58:27 -0800

    completed test 7.

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -23,38 +23,81 @@ test('renders the contact form header', ()=> {
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
     render (<ContactForm/>)
-    const firstname = screen.getByPlaceholderText('Edd');
-    userEvent.type(firstname, 'Nit');
-    const fnameerror = screen.findByText('firstname must have atleast 5 characters');
-    expect(fnameerror).toBeTruthy();
+    const firstName = screen.getByPlaceholderText('Edd');
+    userEvent.type(firstName, 'Nit');
+    const fnameError = screen.findByText('firstname must have atleast 5 characters');
+    expect(fnameError).toBeTruthy();
     
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
     render (<ContactForm/>)
-    const subButton = screen.getByRole('button');
-    userEvent.click(subButton);
+
+    const submitButton = screen.getByRole('button');
+    userEvent.click(submitButton);
     
-    const fnameerror = screen.findByText('firstname must have atleast 5 characters');
-    expect(fnameerror).toBeTruthy();
+    const fnameError = screen.findByText('firstName must have atleast 5 characters.');
+    expect(fnameError).toBeTruthy();
 
-    const lnameerror = screen.findByText('lastname is required');
-    expect(lnameerror).toBeTruthy();
+    const lnameError = screen.findByText('lastName is required.');
+    expect(lnameError).toBeTruthy();
 
-    const emailerror = screen.findByText('email must be a valid email address');
-    expect(emailerror).toBeTruthy();
+    const emailError = screen.findByText('email must be a valid email address.');
+    expect(emailError).toBeTruthy();
     
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
+
+    render (<ContactForm/>)
+
+    const firstName = screen.getByPlaceholderText('Edd');
+    userEvent.type(firstName, 'Nithya');    
+
+    const lastName = screen.getByPlaceholderText('Burke');
+    userEvent.type(lastName, 'Narayanan');    
+
+    const submitButton = screen.getByRole('button');
+    userEvent.click(submitButton);
+
+    const emailError = screen.findByText('email must be a valid email address.');
+    expect(emailError).toBeTruthy();
+
     
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
-});
+
+    render (<ContactForm/>)
+
+    const firstName = screen.getByPlaceholderText('Edd');
+    userEvent.type(firstName, 'Nithya');    
+
+    const lastName = screen.getByPlaceholderText('Burke');
+    userEvent.type(lastName, 'Narayanan');    
+
+    const emailAddress = screen.getByPlaceholderText('bluebill1049@hotmail.com')
+    userEvent.type(emailAddress, 'test')
+
+    const emailError = screen.findByText('email must be a valid email address.');
+    expect(emailError).toBeTruthy();
+
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
+    
+    render (<ContactForm/>)
+
+    const firstName = screen.getByPlaceholderText('Edd');
+    userEvent.type(firstName, 'Nithya');  
+
+    const lnameError = screen.findByText('lastName is required.');
+    expect(lnameError).toBeTruthy();
+
+    const emailAddress = screen.getByPlaceholderText('bluebill1049@hotmail.com')
+    userEvent.type(emailAddress, 'abcxyz@test.com')
+
+    const submitButton = screen.getByRole('button');
+    userEvent.click(submitButton);
     
 });
 
